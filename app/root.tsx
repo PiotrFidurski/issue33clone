fcb3c60ec17d80de35968de158c8720e14af819a
Piotr Fidurski <crunor55@hotmail.com>
2022-01-06 12:53:11 +0100

    navbar & start

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from "remix";
+import { Logo } from "./components/Logo";
+import fontStyles from "./styles/font.css";
 import styles from "./styles/root.css";
 
 export const meta: MetaFunction = () => {
@@ -14,7 +16,38 @@ export const meta: MetaFunction = () => {
 };
 
 export const links: LinksFunction = () => {
-  return [{ href: styles, rel: "stylesheet" }];
+  return [
+    { href: styles, rel: "stylesheet" },
+    { href: fontStyles, rel: "stylesheet" },
+    {
+      rel: "preload",
+      as: "font",
+      href: "/fonts/RedHatDisplay-Black.woff2",
+      type: "font/woff2",
+      crossOrigin: "anonymous",
+    },
+    {
+      rel: "preload",
+      as: "font",
+      href: "/fonts/RedHatDisplay-Black.woff",
+      type: "font/woff",
+      crossOrigin: "anonymous",
+    },
+    {
+      rel: "preload",
+      as: "font",
+      href: "/fonts/RedHatDisplay-Regular.woff2",
+      type: "font/woff2",
+      crossOrigin: "anonymous",
+    },
+    {
+      rel: "preload",
+      as: "font",
+      href: "/fonts/RedHatDisplay-Regular.woff",
+      type: "font/woff",
+      crossOrigin: "anonymous",
+    },
+  ];
 };
 
 export default function App() {
@@ -27,7 +60,40 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <div>
+          <a href="#main-content" className="button skip-nav-link">
+            Skip to content
+          </a>
+          <header role="banner" className="main-header">
+            <div className="wrapper">
+              <div className="main-header__inner">
+                <a href="/" aria-label="Issue 33 - home" className="logo">
+                  <Logo />
+                </a>
+                <nav aria-label="Primary navigation" className="nav">
+                  <ul className="nav__list">
+                    <li>
+                      <a href="/">Home</a>
+                    </li>
+                    <li>
+                      <a href="/">About</a>
+                    </li>
+                    <li>
+                      <a href="/">Work</a>
+                    </li>
+                    <li>
+                      <a href="/">Blog</a>
+                    </li>
+                    <li>
+                      <a href="/">Contact</a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </header>
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
         {process.env.NODE_ENV === "development" && <LiveReload />}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,4 @@
 import { LinksFunction } from "remix";
-import { Logo } from "~/components/Logo";
 import styles from "../styles/index.css";
 
 export const links: LinksFunction = () => {
@@ -8,8 +7,29 @@ export const links: LinksFunction = () => {
 
 export default function Index() {
   return (
-    <div className="dot-shadow">
-      <Logo />
-    </div>
+    <main id="main-content">
+      <div className="wrapper">
+        <article className="start">
+          <div className="start__header">
+            <h1>
+              Digital Marketing is our
+              <em>Bread & Butter</em>
+            </h1>
+          </div>
+          <div className="start__content">
+            <p>
+              Let us help you create the perfect campaign with our multi-faceted
+              team of talented creatives.
+            </p>
+            <a href="/" className="button">
+              see our work
+            </a>
+          </div>
+          <div className="start__media">
+            <img src="/images/toast.jpg" width="100%" height="100%" />
+          </div>
+        </article>
+      </div>
+    </main>
   );
 }
